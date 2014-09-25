eac94ab3523b66889154d7dfc681f74b7f04bd2d
praveenchand <saipraveenchand@gmail.com>
2014-09-25 10:18:49 +0530

    router

--- server/router.js
+++ server/router.js
@@ -3,6 +3,13 @@ var passport = require('passport');
 
 module.exports  = function router(app){
 	
+/*	app.get('/', function(req,res){
+		res.render('layout');
+	});
+	
+	app.get('views/signin', function(req,res){
+		res.render('signin.jade');
+	});*/
 
 	app.get('/auth/google', passport.authenticate('google',{scope:['profile','email']}));
 	
@@ -24,6 +31,17 @@ module.exports  = function router(app){
 	app.get('/auth/twitter/callback', 
 			  passport.authenticate('twitter', { successRedirect: '/',
 			                                     failureRedirect: '/' }));
+	
+	app.post('/login',
+			  passport.authenticate('local-signin', { successRedirect: '/profile',
+			                                   failureRedirect: '/signin',
+			                                   failureFlash : true });
+	
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect : '/profile', // redirect to the secure profile section
+		failureRedirect : '#/signup', // redirect back to the signup page if there is an error
+		failureFlash : true // allow flash messages
+	}));
 	
 	app.get('/logout', function(req, res){
 		  req.logout();
