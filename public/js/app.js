713063bc858d3661dabe0645f7eee68bb2cb3603
praveenchand <saipraveenchand@gmail.com>
2014-09-26 20:18:34 +0530

    commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('nodeBlog', ['ngRoute']);
+var app = angular.module('nodeBlog', ['ngRoute','ui.bootstrap','ngResource']);
 
 app.config(['$routeProvider', '$locationProvider',function($routeProvider,$locationProvider){
 
@@ -12,9 +12,22 @@ app.config(['$routeProvider', '$locationProvider',function($routeProvider,$locat
         templateUrl: "views/signup.html"
     })
     .otherwise({
-        redirectTo: "/signin"
-      });
+    	redirectTo:"/signin"
+    })
+
 }]);
 
+
+
+app.controller('ProfileCntrl',['$scope','$http',
+		function($scope,$http){
+		$scope.username = "";
+		   $http({method:'GET',url:"/auth/info"}).
+		    	success(function(data,status){
+		    		$scope.username = data.email;
+		    		console.log(data);
+		    	})
+	}
+]);
 
 
